6eb51545f9247b092b4d14a1ef508565bddfbe8d
elvira88 <elvira_alvarez1988@hotmail.com>
2018-02-02 17:29:55 +0100

    continuacion ejercicio composicion

--- javaScript/composicion.js
+++ javaScript/composicion.js
@@ -45,44 +45,65 @@ var Alumno = function(nombre="",dni="",tlf="",email="",notas=[]){
         this.notas = notas;
     };
     this.meteNotas=function (notas){
-        this.notas.push();
+        this.notas.push(notas);
     }
 };
-
 /*
 17.- Crea un método que te haga la media de las notas de curso (todas las notas del array de notas)
 */
-   
+function notaMedia(){
+    var acum = 0;
+    for (i in this.notas){
+        var acum = acum+ this.notas[i]/this.notas.length;
+        console.log(notas[i]);
+    }
+    return acum;
+}
 /*
 3.- Crea un objeto de la clase Alumno
 */
-
+var alum = new Alumno; 
+console.log(alum);
 /*
 6.- Crea otro objeto de la clase Alumno pero utilizando el nuevo constructor de la clase, pasando los 4 parámetros
 */
-
+var alum2 = new Alumno("Pepe","71166456X","912825455","alum2@alumno.com");
+console.log(alum2);
 /*
 7.- Crea la clase Nota que tenga dos atributos: valor, asignatura
 9.- Mete los getter y los setter de la clase Nota
-10.- Modifica el constructor de la clase Nota para que admita 2 parámetros, uno para propiedad. Los parámetros deben poder ser opcionales.
+10.- Modifica el constructor de la clase Nota para que admita 2 parámetros, uno para cada propiedad. Los parámetros deben poder ser opcionales.
 */
-
+var Nota = function(valor="",asignatura=""){
+    this.valor = valor;
+    this.asignatura = asignatura;
+    this.getValor = function(){
+        return this.valor;
+    };
+    this.setValor = function (valor){
+        this.valor = valor;
+    };
+    this.getAsignatura = function(){
+        return this.asignatura;
+    };
+    this.setAsignatura = function (asignatura){
+        this.asignatura = asignatura;
+    };
+}
 /*
 8.- Crea un objeto de la clase Nota
 */
-
+var nota1 = new Nota;
 /*
  
 11.- Crea un objeto de la clase Nota pero pasando los dos parámetros en su creación
 */
-
+var nota2 = new Nota("8","Matemáticas");
 /*
 16.- Crea un nuevo objeto Nota e introduce este nuevo objeto en un objeto de tipo Alumno mediante el método definido en el anterior ejercicio
 */
-
-/*
-*/
-
+var nota3 = new Nota ("5","Francés");
+alum2.meteNotas(nota3);
 /*
 18.- Haz un programa que sea capaz de decirte si el alumno ha aprobado o no el curso.
 */
